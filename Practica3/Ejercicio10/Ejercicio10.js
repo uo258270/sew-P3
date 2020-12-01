1449a54a6fd2c35f47417591a0a6c6a8fa78e0db
nuria <uo258270@uniovi.es>
2020-12-01 22:36:23 +0100

    h

--- Practica3/Ejercicio10/Ejercicio10.js
+++ Practica3/Ejercicio10/Ejercicio10.js
@@ -8,23 +8,47 @@ class News{
         
     }
 
+    nuevoElemento(tipoElemento, texto, insertarAntesDe) {
+        var elemento = document.createElement(tipoElemento);
+        elemento.innerHTML = texto;
+        $(insertarAntesDe).before(elemento);
+    }
+
+
     cargarNoticias(noticias) {
 
-        var articulos = noticias.articles;
-        var total = noticias.totalResults;
+        for(let i =1; i<noticias.articles.length; i++){
+            this.nuevoElemento("article", "", "footer", i);
+            $.ajax({
+                dataType: "xml",
+                url: this.url,
+                method: 'GET',
+                success: function(datos){
+                   $('pre').text(JSON.stringify(datos, null, 2));
 
-        articulos.forEach(articulo => {
+                   var html = "<article>";
+                   html += '<h2>' + datos.articles[i].title + '</h2>';
+                   //html+= '<img src="' + articulo.urlToImg +'"/>';
+                   html += '<p>' + datos.articles[i].content + '</p>';
+                   html += '<p>' + datos.articles[i].description + '</p>';
+                   //html += '<ref>' + articulo.url + '</ref>';
+                   
+                   html += '</article>';
+                   $('#resultado div').append(html);
 
-            var html = '<article>';
-            html += '<h1>' + articulo.title + '</h1>';
-            html+= '<img src="' + articulo.urlToImg +'"/>';
-            html += '<p>' + articulo.content + '</p>';
-            html += '<p>' + articulo.description + '</p>';
-            //html += '<ref>' + articulo.url + '</ref>';
-            
-            html += '</article>';
-            $('#resultado div').append(html);
-        });
+                },
+                error:function(){
+                    $("h3").html("error, no se puede obtener JSON");
+                    $('pre').remove();
+                    $('p').remove();
+                }
+
+        
+            });
+        
+        
+        
+        }
     }
 
    
